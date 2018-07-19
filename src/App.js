df3ea34ee6964345e7f19dc741408998e2b1b2d3
Gabe Hollombe <gabe@avantbard.com>
2018-07-19 09:36:00 +0800

    Added creating events mutation and form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Amplify, { graphqlOperation, Auth } from 'aws-amplify';
+import Amplify, { API, graphqlOperation, Auth } from 'aws-amplify';
 import aws_exports from './aws-exports'; // specify the location of aws-exports.js file on your project
 import { Connect, withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
 
 Amplify.configure(aws_exports);
+
+class EventForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      description: "",
+      where: "", 
+      when: ""
+    }
+  }
+
+  handleChange = (event) => {
+    let update = {};
+    update[event.target.name] = event.target.value;
+    this.setState(update);
+  }
+
+  addEvent = async () => {
+    const CreateEvent = `mutation CreateEvent($name: String!, $when: String!, $where: String!, $description: String!) {
+      createEvent(name: $name, when: $when, where: $where, description: $description) {
+        id
+        name
+        where
+        when
+        description
+      }
+    }`;
+    
+    const newEvent = await API.graphql(graphqlOperation(CreateEvent, this.state));
+    console.log("Event created", newEvent);
+    this.setState({name: "", description: "", where: "", when: ""});
+  }
+
+  render = () => {
+    return (
+      <div>
+        <input type="text" name="name" placeholder="Name" onChange={this.handleChange} />
+        <input type="text" name="description" placeholder="Description" onChange={this.handleChange} />
+        <input type="text" name="where" placeholder="Where" onChange={this.handleChange} />
+        <input type="text" name="when" placeholder="When" onChange={this.handleChange} />
+        <button onClick={this.addEvent}>Add Event</button>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   render() {
@@ -29,14 +75,18 @@ class App extends Component {
   }`;
 
   return (
-      <Connect query={graphqlOperation(ListEvents)}>
-          {({ data, errors, loading}) => {
-            if (loading) return <div>Loading...</div>;
-            if (errors.length > 0) return <div>JSON.stringify(errors)</div>;
-            if (!data.listEvents) return;
-            return <ListView events={data.listEvents.items} />
-          }}
-      </Connect>
+      <div>
+        <EventForm />
+
+        <Connect query={graphqlOperation(ListEvents)}>
+            {({ data, errors, loading}) => {
+              if (loading) return <div>Loading...</div>;
+              if (errors.length > 0) return <div>JSON.stringify(errors)</div>;
+              if (!data.listEvents) return;
+              return <ListView events={data.listEvents.items} />
+            }}
+        </Connect>
+      </div>
   )
   }
 }
